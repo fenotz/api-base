baeabbcc744ef0adc7b0ef077e9dbe2e91608102
fenox <manuel.martinez.r@hotmail.com>
2024-10-11 19:02:09 -0600

    Service Provider add exceptions

--- src/BaseApiServiceProvider.php
+++ src/BaseApiServiceProvider.php
@@ -2,13 +2,12 @@
 namespace Fenox\ApiBase;
 
 use Fenox\ApiBase\Console\MakeApiModel;
-use Fenox\ApiBase\Exceptions\Handler;
 use Fenox\ApiBase\Middleware\ForceJsonResponse;
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Http\Request;
+use Throwable;
 use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
@@ -16,78 +15,67 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BaseApiServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
-
         if ($this->app->runningInConsole()) {
             $this->commands([
                 MakeApiModel::class,
             ]);
         }
+
         // Registrar el middleware de JSON
-        //$this->app['router']->aliasMiddleware('force.json', \Fenox\ApiBase\Middleware\ForceJsonResponse::class);
         $kernel = $this->app->make(Kernel::class);
         $kernel->pushMiddleware(ForceJsonResponse::class);
-        // Registrar la lógica de excepciones
-        $this->registerExceptionHandling();
-        $this->app->singleton(ExceptionHandler::class, Handler::class);
-    }
 
+        // Registrar el manejo de excepciones para respuestas JSON
+        $this->app->afterResolving(
+            Exceptions::class,
+            function ($exceptions) {
+                $exceptions->render(function (Throwable $e, Request $request) {
+                    // Manejar excepciones y forzar respuestas en JSON
+                    if ($e instanceof QueryException && $e->getCode() === '42S02') {
+                        return response()->json([
+                            'message' => 'The requested table or view does not exist in the database. Run php artisan:migrate?',
+                        ], 500);
+                    }
 
-    /**
-     *  Registra la lógica de manejo de excepciones
-     *
-     * @return void
-     * @throws BindingResolutionException
-     */
-    protected function registerExceptionHandling(): void
-    {
-        $this->app->make('Illuminate\Contracts\Debug\ExceptionHandler')->renderable(function ($exception, Request $request) {
-            // Manejo de errores
-            if ($exception instanceof QueryException && $exception->getCode() === '42S02') {
-                return response()->json([
-                    'message' => 'The requested table or view does not exist in the database. Run php artisan:migrate?',
-                ], 500);
-            }
+                    if ($e instanceof QueryException && $e->getCode() === 'HY000') {
+                        return response()->json([
+                            'message' => 'A required field is missing or has no default value. (Check your Request Rules)',
+                        ], 422);
+                    }
 
-            if ($exception instanceof QueryException) {
-                if ($exception->getCode() === 'HY000') {
+                    if ($e instanceof ValidationException) {
+                        return response()->json([
+                            'status' => 'error',
+                            'message' => 'Validation failed',
+                            'errors' => $e->errors(),
+                        ], 422);
+                    }
+
+                    if ($e instanceof AuthenticationException) {
+                        return response()->json([
+                            'message' => 'Unauthenticated',
+                        ], 401);
+                    }
+
+                    if ($e instanceof NotFoundHttpException) {
+                        return response()->json([
+                            'message' => 'Route not found',
+                        ], 404);
+                    }
+
+                    if ($e instanceof QueryException && $e->errorInfo[1] == 1062) {
+                        return response()->json([
+                            'message' => 'The field has already been taken.',
+                        ], 422);
+                    }
+
                     return response()->json([
-                        'message' => 'A required field is missing or has no default value. (Check your Request Rules)',
-                    ], 422);
-                }
+                        'message' => $e->getMessage(),
+                    ], $e->getCode() ?: 500);
+                });
             }
-
-            if ($exception instanceof ValidationException) {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Validation failed',
-                    'errors' => $exception->errors(),
-                ], 422);
-            }
-
-            if ($exception instanceof AuthenticationException) {
-                return response()->json([
-                    'message' => 'Unauthenticated',
-                ], 401);
-            }
-
-            if ($exception instanceof NotFoundHttpException) {
-                return response()->json([
-                    'message' => 'Route not found',
-                ], 404);
-            }
-
-            if ($exception instanceof QueryException && $exception->errorInfo[1] == 1062) {
-                return response()->json([
-                    'message' => 'The field has already been taken.',
-                ], 422);
-            }
-
-            return response()->json([
-                'message' => $exception->getMessage(),
-            ], $exception->getCode() ?: 500);
-        });
+        );
     }
 }
-
